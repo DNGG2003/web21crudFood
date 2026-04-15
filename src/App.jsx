9bfe48b733d61a6601be78a2263f5f020f5d1161
Diego Giménez <gimenezd15@gmail.com>
2026-04-15 13:46:49 -0300

    Agregue las rutas en el componente App y agregue estilos en el app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router";
+import Menu from "./components/shared/Menu";
+import Footer from "./components/shared/Footer";
+import Home from "./components/views/Home/Home";
+import DetalleProducto from "./components/views/Producto/DetalleProducto";
+import Login from "./components/views/Login";
+import Administrador from "./components/views/Administrador";
+import FormularioProducto from "./components/views/Producto/FormularioProducto";
+import Error404 from "./components/views/Error404";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Menu></Menu>
+        <main>
+          <Routes>
+            {/* El path es la ubicacion */}
+            <Route path="/" element={<Home></Home>} />
+            <Route
+              path="/detalle"
+              element={<DetalleProducto></DetalleProducto>}
+            />
+            <Route path="/login" element={<Login></Login>} />
+            <Route
+              path="/administrador"
+              element={<Administrador></Administrador>}
+            />
+            <Route
+              path="/crear"
+              element={<FormularioProducto></FormularioProducto>}
+            />
+            {/* Utilizamos el id porque en este caso vamos a estar reutilizando el FormularioProducto, pero la diferencia va a ser que este formulario ya va a venir cargado con datos */}
+            <Route
+              path="/editar/:id"
+              element={<FormularioProducto></FormularioProducto>}
+            />
+            <Route path="*" element={<Error404></Error404>} />
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
